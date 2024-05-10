0eab9e3ec5e7e48d2eefb6e6aa289373b6cb1372
leejunghyeok <wndgur76@naver.com>
2024-05-10 19:30:33 +0900

    script test

--- script.sh
+++ script.sh
@@ -1,11 +1,5 @@
-# if [ "$#" -ne 1 ]; then
-#     echo "사용법: $0 <commit_message>"
-#     exit 1
-# fi
-
 # generate temp file
 html=$(mktemp)
-
 cp _header.html "$html"
 
 # get all .md files
@@ -16,11 +10,3 @@ done
 
 cat _footer.html >> "$html"
 cp "$html" index.html
-
-
-# commit_message="$1"
-
-# git add .
-# git commit -m "$commit_message"
-# git push origin main
-# echo "Push complete!"
