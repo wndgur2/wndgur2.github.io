f50e73330852394325313d953f0a8a98a66b8fc1
leejunghyeok <wndgur76@naver.com>
2024-05-10 18:06:55 +0900

    test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+// get all .md files
+
+const fs = require('fs');
+const path = require('path');
+
+const getFiles = (dir, files_) => {
+    files_ = files_ || [];
+    const files = fs.readdirSync(dir);
+    for (const i in files) {
+        const name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory())
+            getFiles(name, files_);
+        else if (name.includes('.md')) 
+            files_.push(name);
+    }
+    return files_;
+};
+
+const files = getFiles('./');
+console.log(files);
