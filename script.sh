e990167c1172cbe2b18b457d6a0e5219d43205ee
leejunghyeok <wndgur76@naver.com>
2024-05-11 01:00:24 +0900

    script test

--- script.sh
+++ script.sh
@@ -5,7 +5,9 @@ cp _header.html "$html"
 # get all .md files
 find . -name '*.md' -print | while read file; do
     # get file value
+    "<div class='md'>" >> "$html"
     pandoc "$file" >> "$html"
+    "</div>" >> "$html"
 done
 
 cat _footer.html >> "$html"
