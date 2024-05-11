6c1883619a80afd6a360a1b0d6fd01524182afab
leejunghyeok <wndgur76@naver.com>
2024-05-12 02:13:39 +0900

    new script

--- script.sh
+++ script.sh
@@ -2,11 +2,28 @@
 html=$(mktemp)
 cp _header.html "$html"
 
+files=()
 # get all .md files
-find . -name '*.md' -print | while read file; do
-    echo '<div class="md">' >> "$html"
-    pandoc "$file" >> "$html"
-    echo '</div>' >> "$html"
+while IFS= read -r file; do
+    files+=("$file")
+    echo "Found $file"
+    echo "${files[@]}"
+done < <(find . -name '*.md' -print)
+
+echo "Files"
+echo "${files[@]}"
+# sort files by date
+IFS=$'\n' files=($(stat -f '%SB %N' "${files[@]}" | sort -n -r | cut -d ' ' -f5-))
+
+echo "Sorted files"
+echo "${files[@]}"
+
+# convert each file to html
+for file in "${files[@]}"; do
+    echo "Converting $file"
+    echo "<div class='md'>" >> "$html"
+    pandoc -f markdown -t html "$file" >> "$html"
+    echo "</div>" >> "$html"
 done
 
 cat _footer.html >> "$html"
