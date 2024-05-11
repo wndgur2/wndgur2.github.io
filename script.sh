8ec3b41359ca0677bb21cae74df3f47e57774fe7
leejunghyeok <wndgur76@naver.com>
2024-05-12 01:32:32 +0900

    f

--- script.sh
+++ script.sh
@@ -3,9 +3,7 @@ html=$(mktemp)
 cp _header.html "$html"
 
 # get all .md files
-# TODO: 각 파일 모듈화
 find . -name '*.md' -print | while read file; do
-    # get file value
     echo '<div class="md">' >> "$html"
     pandoc "$file" >> "$html"
     echo '</div>' >> "$html"
