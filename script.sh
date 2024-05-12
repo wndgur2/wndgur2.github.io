ba43bc9451dd13064b4cf61f0cd0398c0275cc00
leejunghyeok <wndgur76@naver.com>
2024-05-12 09:25:23 +0900

    old script

--- script.sh
+++ script.sh
@@ -6,17 +6,7 @@ files=()
 # get all .md files
 while IFS= read -r file; do
     files+=("$file")
-    echo "Found $file"
-    echo "${files[@]}"
 done < <(find . -name '*.md' -print)
-
-echo "Files"
-echo "${files[@]}"
-# sort files by date
-IFS=$'\n' files=($(stat -f '%SB %N' "${files[@]}" | sort -n -r | cut -d ' ' -f5-))
-
-echo "Sorted files"
-echo "${files[@]}"
 
 # convert each file to html
 for file in "${files[@]}"; do
