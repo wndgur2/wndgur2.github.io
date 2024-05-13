ce89aeb434f2b6a8d6e12abd8388e72fea3557f8
leejunghyeok <wndgur76@naver.com>
2024-05-13 18:11:36 +0900

    t

--- script.sh
+++ script.sh
@@ -1,30 +1,30 @@
+#!/bin/bash
+
 # generate temp file
 html=$(mktemp)
 cp _header.html "$html"
-
 files=()
+
 # get all .md files
 while IFS= read -r file; do
     files+=("$file")
 done < <(find . -name '*.md' -print)
 
-# sort files by name
-IFS=$'\n' files=($(sort -n <<<"${files[*]}"))
-
 dates_files=()
 for file in "${files[@]}"; do
     date=$(stat -t '%F' $file | cut -d ' ' -f 12)
-    dates_files+=("$date $file")
+    dates_files+=("$date:$file")
+    echo "Detected $date:$file"
 done
 
 sorted_dates_files=($(printf "%s\n" "${dates_files[@]}" | sort -r -k1,1))
 
 # convert each file to html
-for file in "${sorted_dates_files[@]}"; do
-    file=$(echo $file | cut -d ' ' -f 2)
-    echo "Converting $file"
+for f in "${sorted_dates_files[@]}"; do
+    echo "Converting $f"
+    f=$(echo $f | cut -d ':' -f 2)
     echo "<div class='block $extension'>" >> "$html"
-    pandoc -f markdown -t html "$file" >> "$html"
+    pandoc -f markdown -t html "$f" >> "$html"
     echo "</div>" >> "$html"
 done
 
