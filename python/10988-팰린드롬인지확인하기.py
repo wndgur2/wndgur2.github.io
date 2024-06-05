56131195b5387f16ef371536cfc7c4e8367facab
leejunghyeok <wndgur76@naver.com>
2024-06-05 15:00:37 +0900

    python practice

---
+++ python/10988-팰린드롬인지확인하기.py
@@ -0,0 +1,7 @@
+word = input()
+backword = word[::-1]
+
+if word==backword:
+    print(1)
+else:
+    print(0)
---
+++ python/2444-별찍기.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n):
+    print(' '*(n-i-1), end='')
+    print('*'*(i*2+1))
+for i in range(n-1):
+    print(' '*(i+1), end='')
+    print('*'*((n-i-1)*2-1))
