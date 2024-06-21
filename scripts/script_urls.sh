df7a8155c58038c0520640881a4bdff81eae9233
leejunghyeok <wndgur76@naver.com>
2024-06-21 13:31:56 +0900

    scripts 정리

--- scripts/script_urls.sh
+++ scripts/script_urls.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # This script creates a list of URLs of md files.
-# The list is saved in a file named posts/urls.json
+# The list is saved in a file named ./urls.json
 
 # Create the posts directory if it does not exist
 mkdir -p posts
 
 # Create the urls.json file
-echo "[" > posts/urls.json
+echo "[" > ./urls.json
 
 # Find all md files in the posts directory
 find . -name "*.md" | while read file; do
@@ -20,9 +20,9 @@ find . -name "*.md" | while read file; do
   fi
 
   # Add the URL to the urls.json file
-  echo "  \"$url\"," >> posts/urls.json
+  echo "  \"$url\"," >> ./urls.json
 done
-echo "  \"\"" >> posts/urls.json
+echo "  \"\"" >> ./urls.json
 # Close the urls.json file
-echo "]" >> posts/urls.json
+echo "]" >> ./urls.json
 
