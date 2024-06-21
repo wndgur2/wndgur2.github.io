9d6e0cfdc703ebdd78a403c53d472f1a36c72c8a
leejunghyeok <wndgur76@naver.com>
2024-06-21 13:34:34 +0900

    스크립트 정리

--- scripts/scrapUrl.sh
+++ scripts/scrapUrl.sh
@@ -3,14 +3,11 @@
 # This script creates a list of URLs of md files.
 # The list is saved in a file named ./urls.json
 
-# Create the posts directory if it does not exist
-mkdir -p posts
-
 # Create the urls.json file
 echo "[" > ./urls.json
 
 # Find all md files in the posts directory
-find . -name "*.md" | while read file; do
+find .. -name "*.md" | while read file; do
   # Get the URL of the file
   url=$(echo $file)
 
@@ -18,6 +15,9 @@ find . -name "*.md" | while read file; do
   if [[ $url == *"node_modules"* ]]; then
     continue
   fi
+
+  # remove first path
+  url=$(echo $url | cut -d'/' -f2-)
 
   # Add the URL to the urls.json file
   echo "  \"$url\"," >> ./urls.json
