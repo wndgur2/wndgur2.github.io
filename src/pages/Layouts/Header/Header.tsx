9b746d6b2336b6b3ea38c8634703027a9c731f72
leejunghyeok <wndgur76@naver.com>
2024-07-13 13:03:00 +0900

    header scroll event

--- src/pages/Layouts/Header/Header.tsx
+++ src/pages/Layouts/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent, useContext, useEffect, useRef } from "react";
 import './Header.css';
 import Theme from "./Theme/Theme";
 import SearchBar from "./SearchBar/SearchBar";
@@ -7,9 +7,29 @@ import HomeButton from "./HomeButton/HomeButton";
 
 const Header: FunctionComponent = () => {
     const { isDark, setIsDark } = useContext(DeviceContext);
+    const headerRef = useRef<HTMLDivElement>(null);
+
+    // hide header when scrolling down
+    let lastScrollTop = 0;
+    window.addEventListener('scroll', () => {
+        if (!headerRef.current) return;
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        console.log(currentScroll, lastScrollTop);
+        if (currentScroll > 0 && currentScroll > lastScrollTop) {
+            headerRef.current.classList.add('hide');
+        } else if (currentScroll === 0) {
+            headerRef.current.classList.remove('hide');
+            headerRef.current.classList.add('top');
+        }
+        else {
+            headerRef.current.classList.remove('hide');
+            headerRef.current.classList.remove('top');
+        }
+        lastScrollTop = currentScroll;
+    });
 
     return (
-        <div id="header">
+        <div id="header" ref={headerRef}>
             <SearchBar />
             <HomeButton />
             <Theme isChecked={isDark} handleChange={() => {
