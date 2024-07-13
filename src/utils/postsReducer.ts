7b03bcde1507bb58c1f67e4826ff4f32435a437f
leejunghyeok <wndgur76@naver.com>
2024-07-13 12:44:03 +0900

    prettier

--- src/utils/postsReducer.ts
+++ src/utils/postsReducer.ts
@@ -1,12 +1,16 @@
 import PostAction from "../types/PostsAction";
 import { _Post } from "../types/_Post";
 
-
 const postsReducer = (state: _Post[], action: PostAction): _Post[] => {
     switch (action.type) {
-        case 'INSERT_POST':
+        case "INSERT_POST":
             // if post already exists, return state
-            if(state.find((prevPost:_Post) => prevPost.id === action.payload.id)) return state;
+            if (
+                state.find(
+                    (prevPost: _Post) => prevPost.id === action.payload.id
+                )
+            )
+                return state;
             // insert post sorted by date_started
             // find index by binary search
             let left = 0;
@@ -14,19 +18,24 @@ const postsReducer = (state: _Post[], action: PostAction): _Post[] => {
             let index = 0;
             while (left <= right) {
                 index = Math.floor((left + right) / 2);
-                if (state[index].date_started > action.payload.date_started){
+                if (state[index].date_started > action.payload.date_started) {
                     left = index + 1;
                     index = left;
-                }
-                else if (state[index].date_started < action.payload.date_started)
+                } else if (
+                    state[index].date_started < action.payload.date_started
+                )
                     right = index - 1;
                 else break;
             }
-            return [...state.slice(0, index), action.payload, ...state.slice(index)];
+            return [
+                ...state.slice(0, index),
+                action.payload,
+                ...state.slice(index),
+            ];
 
         default:
             return state;
     }
-}
+};
 
 export default postsReducer;
