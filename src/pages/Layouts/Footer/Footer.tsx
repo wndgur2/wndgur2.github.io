3cba924aed50944e968558597353c46c79f6f26b
wndgur2 <wndgur76@naver.com>
2024-07-15 08:54:18 +0900

    footer, iconLink 개선

--- src/pages/Layouts/Footer/Footer.tsx
+++ src/pages/Layouts/Footer/Footer.tsx
@@ -2,23 +2,31 @@ import { FunctionComponent } from "react";
 import './Footer.css';
 import { IoLogoGithub } from "react-icons/io";
 import { HiOutlineMail } from "react-icons/hi";
+import { FaYoutube } from "react-icons/fa";
 import IconLink from "../../../components/IconLink/IconLink";
+
 
 const Footer: FunctionComponent = () => {
     return (
         <footer>
             <div className="links">
                 <div className="col">
-                    <IconLink icon={<IoLogoGithub size={44} />} url={"https://github.com/wndgur2"} />
+                    <IconLink icon={<IoLogoGithub size={42} />} url={"https://github.com/wndgur2"} />
                     <div className="seperator" />
                     <small>Github</small>
-                    <small>wndgur2</small>
+                    <small>@wndgur2</small>
                 </div>
                 <div className="col">
-                    <IconLink icon={<HiOutlineMail size={42} />} url={"https://mail.google.com/mail/?view=cm&to=dkandjsl@gmail.com"} />
+                    <IconLink icon={<HiOutlineMail size={38} />} url={"https://mail.google.com/mail/?view=cm&to=dkandjsl@gmail.com"} />
                     <div className="seperator" />
                     <small>Email</small>
                     <small>dkandjsl@gmail.com</small>
+                </div>
+                <div className="col">
+                    <IconLink icon={<FaYoutube size={32} />} url={"https://www.youtube.com/@junghyeok_lee6710"} />
+                    <div className="seperator" />
+                    <small>Youtube</small>
+                    <small>Demo videos / vlog</small>
                 </div>
             </div>
         </footer>
