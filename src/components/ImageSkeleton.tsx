3596edf78297ed1b2fb878ca94be91f96606fae4
leejunghyeok <wndgur76@naver.com>
2024-12-08 16:23:27 +0900

    refactor: radius vars

--- src/components/ImageSkeleton.tsx
+++ src/components/ImageSkeleton.tsx
@@ -13,36 +13,29 @@ const ImageSkeleton: FunctionComponent<ImageSkeletonProps> = ({ props }) => {
     if (!imgRef.current) return
     const image = imgRef.current
 
-    if (!image) return
     image.src = props.src
     imgRef.current.classList.add('loading')
     image.onload = () => {
-      if (!skeletonRef.current) return
-      skeletonRef.current.remove()
-      if (!imgRef.current) return
-      imgRef.current.classList.replace('loading', 'loaded')
+      skeletonRef.current?.remove()
+      imgRef.current?.classList.replace('loading', 'loaded')
     }
   }, [props.src])
 
-  useEffect(() => {
-    if (!skeletonRef.current) return
-    // set random animation duration
-    const duration = Math.random() * 2
-    skeletonRef.current.style.setProperty('animation-delay', `${duration}s`)
-
-  }, [skeletonRef])
-
   return (
-    <div className='image-wrapper'>
+    <div className='image-container'>
       <canvas
         className='image-skeleton'
         ref={ skeletonRef }
       />
-      <img
-        alt=''
-        { ...props }
-        ref={ imgRef }
-      />
+      <div
+        className='image-wrapper'
+      >
+        <img
+          alt=''
+          { ...props }
+          ref={ imgRef }
+        />
+      </div>
     </div>
   )
 }
