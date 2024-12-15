d10424320a5126c881a2631fdab306eb767128fc
leejunghyeok <wndgur76@naver.com>
2024-12-15 16:33:05 +0900

    refactor: format

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { DeviceContext } from './contexts/Device'
-import Layout from './pages/Layouts/Layout'
-import Home from './pages/Home/Home'
-import Post from './pages/Post/PostDetail'
-import SearchResult from './pages/Search/SearchResult'
-import NoPage from './pages/NoPage'
-import usePosts from './hooks/usePosts'
+import { DeviceContext } from '@/contexts/Device'
+import usePosts from '@/hooks/usePosts'
+import SearchResult from '@/pages/Search/SearchResult'
+import NoPage from '@/pages/NoPage'
+import Layout from '@/pages/Layouts/Layout'
+import Home from '@/pages/Home/Home'
+import Post from '@/pages/Post/PostDetail'
 
-export default function App () {
+export default function App() {
   const [isDark, setIsDark] = useState(localStorage.getItem('theme') === 'light' ? false : true)
   usePosts()
   useEffect(() => {
@@ -20,13 +20,28 @@ export default function App () {
 
   return (
     <BrowserRouter>
-      <DeviceContext.Provider value={ { isDark, setIsDark } }>
+      <DeviceContext.Provider value={{ isDark, setIsDark }}>
         <Routes>
-          <Route path='/' element={ <Layout /> }>
-            <Route index element={ <Home /> } />
-            <Route path='/search/:search_text' element={ <SearchResult /> } />
-            <Route path='/post/:post_title' element={ <Post /> } />
-            <Route path='*' element={ <NoPage /> } />
+          <Route
+            path="/"
+            element={<Layout />}
+          >
+            <Route
+              index
+              element={<Home />}
+            />
+            <Route
+              path="/search/:search_text"
+              element={<SearchResult />}
+            />
+            <Route
+              path="/post/:post_title"
+              element={<Post />}
+            />
+            <Route
+              path="*"
+              element={<NoPage />}
+            />
           </Route>
         </Routes>
       </DeviceContext.Provider>
