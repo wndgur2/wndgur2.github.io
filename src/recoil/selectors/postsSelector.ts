9f0934a897170c22009043655091ccd3e6b3fb5e
leejunghyeok <wndgur76@naver.com>
2025-01-02 15:27:17 +0900

    refactor: handle loading state

--- src/recoil/selectors/postsSelector.ts
+++ src/recoil/selectors/postsSelector.ts
@@ -33,15 +33,15 @@ export const searchedPostsSelector = selectorFamily<_Post[], { search_text?: str
     },
 })
 
-export const postSelector = selectorFamily<_Post | undefined, { post_title?: string }>({
+export const postSelector = selectorFamily<_Post | undefined | null, { post_title?: string }>({
   key: 'postSelector',
   get:
     (params) =>
     ({ get }) => {
       const posts = get(postsAtom)
-      // console.log(posts) // TODO : posts가 아직 다 로드되지 않았을 때, loading 반환
-      // 현재 undefined 반환했을 때, 로딩중인지, 없는 post인지 구분할 수 없음
-      return posts.find((post) => post.title === params.post_title)
+      if (!posts.length) return undefined // Handle loading state
+
+      return posts.find((post) => post.title === params.post_title) || null
     },
 })
 
@@ -51,6 +51,8 @@ export const prevPostSelector = selectorFamily<_Post | undefined, { post_title?:
     (params) =>
     ({ get }) => {
       const posts = get(postsAtom)
+      if (!posts.length) return undefined // Handle loading state
+
       const postIndex = posts.findIndex((post) => post.title === params.post_title)
       return posts[postIndex + 1]
     },
@@ -62,6 +64,8 @@ export const nextPostSelector = selectorFamily<_Post | undefined, { post_title?:
     (params) =>
     ({ get }) => {
       const posts = get(postsAtom)
+      if (!posts.length) return undefined // Handle loading state
+
       const postIndex = posts.findIndex((post) => post.title === params.post_title)
       return posts[postIndex - 1]
     },
