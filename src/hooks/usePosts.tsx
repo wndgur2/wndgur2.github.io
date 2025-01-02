fae8696f236d7119701df84d95989f51688d9601
leejunghyeok <wndgur76@naver.com>
2025-01-02 15:28:40 +0900

    refactor: test post fetching hook

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -3,11 +3,12 @@ import { getPosts, getProjects } from '@/api/post'
 import { useSetRecoilState } from 'recoil'
 import { postsAtom } from '@/recoil/atoms/postsAtom'
 
-
-function usePosts () {
-  const setPosts = useSetRecoilState(postsAtom);
+function usePosts() {
+  const setPosts = useSetRecoilState(postsAtom)
+  console.log('rendering usePosts')
 
   useEffect(() => {
+    console.log('getting posts')
     getPosts(setPosts)
     getProjects(setPosts)
   }, [setPosts])
