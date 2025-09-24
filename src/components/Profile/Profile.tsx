d7ce4a763d18f8e197b8cb5448b64092921af7a0
wndgur2 <wndgur76@naver.com>
2025-09-24 15:18:33 +0900

    feat : profile 개선

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,27 +2,33 @@ import { type FunctionComponent } from 'react'
 
 import './Profile.css'
 
-import TagList from '../common/TagList'
+import { useRecoilValue } from 'recoil'
+
+import { postsAtom } from '@/recoil'
+import TagCountList from '../common/TagCountList'
 import ProfileImage from './ProfileImage'
 
 const Profile: FunctionComponent = () => {
-  const stacks = [
-    'typescript',
-    'javascript',
-    'react',
-    'next.js',
-    'express',
-    'css',
-    'java',
-  ]
+  const posts = useRecoilValue(postsAtom)
+  const tagsMap = Array.from(
+    posts.reduce((acc, post) => {
+      post.tags.forEach(tag => {
+        acc.set(tag, (acc.get(tag) ?? 0) + 1)
+      })
+      return acc
+    }, new Map()),
+  )
+  const tags = tagsMap
+    .map(([tag, count]) => ({ tag, count }))
+    .sort((a, b) => b.count - a.count)
 
   return (
     <div className='profile'>
       <ProfileImage />
       <div className='profile-content'>
-        <h3 className='profile-name'>이중혁</h3>
+        <h2 className='profile-name'>이중혁</h2>
         <div className='profile-tags'>
-          <TagList tags={stacks} />
+          <TagCountList tags={tags} />
         </div>
       </div>
     </div>
