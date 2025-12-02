f085fd26547804e2e7a3daebda7c37d1a4a49517
wndgur2 <wndgur76@naver.com>
2025-12-02 17:00:58 +0900

    style: brighter light-dimmed-fg-color, darker light-dimmed-bg-color

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7bc42d0b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v22f0hu9cio"
+    "revision": "0.i3p0h1l3g58"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
